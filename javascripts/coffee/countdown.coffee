0d6fd2b43207b53cfd9ac443cb516bdfa2d1a801
Sparkmasterflex <raymondke99@gmail.com>
2013-09-29 13:35:45 -0400

    More dynamic for interchangable dates
    Added stylesheet link for masters-degree.css
    refactored html for
      -- h1.rtw-logo -> now .title
      -- h2>time -> removed datetime attr
    learn-more link
    
    added loading spinner till calculations are done and added to dom
    $.get for json file with all attributes
    builds dom w/attributes
    
    removed rome total war colors and bg
    
    created stylesheet for masters-degree
      -- colors needed
      -- bg image

--- javascripts/coffee/countdown.coffee
+++ javascripts/coffee/countdown.coffee
@@ -1,21 +1,52 @@
 $ ->
+	$('.flip-front').find('h1, h2, .countdown-container').hide()
+	$('.flip-front').append "<span class='spinner'>Loading</span>"
 	$('a[rel=external]').bind 'click', () -> this.target = "_blank"
+
+	# $('a.twitter').click (e) ->
+	# 	# status = " "
+	# 	# window.location = "http://twitter.com/home?status=#{status}&has"
+	# 	window.location = "http://twitter.com/share?url=http://countdown.ifkeithraymond.com&text=#{$('time.countdown').html()} till Rome Total War 2!&hashtag=RomeTotalWar2"
+	# 	false
+
 	init_countdown()
 
-
 init_countdown = () ->
-	release = moment $('time.release-date').attr('datetime')
-	interval = setInterval () ->
-		seconds = release.diff moment(new Date()), 's'
-		days = Math.floor seconds/86400
-		seconds -= days*86400
-		hours = Math.floor seconds/3600
-		hours = if hours < 10 then "0#{hours}" else hours
-		seconds -= hours*3600
-		minutes = Math.floor seconds/60
-		minutes = if minutes < 10 then "0#{minutes}" else minutes
-		seconds -= minutes*60
-		seconds = if seconds < 10 then "0#{seconds}" else seconds
+	get_countdown_attrs()
+	build_timer()
 
-		$('time.countdown').html "#{days} days #{hours}:#{minutes}:#{seconds}"
+get_countdown_attrs = () ->
+	$.get "/javascripts/graduation.json", (data) ->
+		$.each data, (key, object) ->
+			$('.title').addClass 'logo' if object.logo?
+			$('.title').html key
+
+			$('time.release-date')
+			.attr('datetime', object.date)
+			.html moment(object.date).format("MMMM D, YYYY")
+			
+			$('a.learn-more').attr('href', object.link)
+
+build_timer = () ->
+	$.when($('time.countdown').html get_time_left()).then () ->
+		$('span.spinner').fadeOut 'fast'
+		$('.flip-front').find('h1, h2, .countdown-container').fadeIn 'fast'
+
+	interval = setInterval () ->
+		$('time.countdown').html get_time_left()
 	, 1000
+
+get_time_left = () ->
+	release = moment $('time.release-date').attr('datetime')
+	seconds = release.diff moment(new Date()), 's'
+	days = Math.floor seconds/86400
+	seconds -= days*86400
+	hours = Math.floor seconds/3600
+	hours = if hours < 10 then "0#{hours}" else hours
+	seconds -= hours*3600
+	minutes = Math.floor seconds/60
+	minutes = if minutes < 10 then "0#{minutes}" else minutes
+	seconds -= minutes*60
+	seconds = if seconds < 10 then "0#{seconds}" else seconds
+
+	"#{days} days #{hours}:#{minutes}:#{seconds}"
--- javascripts/countdown.js
+++ javascripts/countdown.js
@@ -1,7 +1,9 @@
 (function() {
-  var init_countdown;
+  var build_timer, get_countdown_attrs, get_time_left, init_countdown;
 
   $(function() {
+    $('.flip-front').find('h1, h2, .countdown-container').hide();
+    $('.flip-front').append("<span class='spinner'>Loading</span>");
     $('a[rel=external]').bind('click', function() {
       return this.target = "_blank";
     });
@@ -9,22 +11,46 @@
   });
 
   init_countdown = function() {
-    var interval, release;
-    release = moment($('time.release-date').attr('datetime'));
+    get_countdown_attrs();
+    return build_timer();
+  };
+
+  get_countdown_attrs = function() {
+    return $.get("/javascripts/graduation.json", function(data) {
+      return $.each(data, function(key, object) {
+        if (object.logo != null) $('.title').addClass('logo');
+        $('.title').html(key);
+        $('time.release-date').attr('datetime', object.date).html(moment(object.date).format("MMMM D, YYYY"));
+        return $('a.learn-more').attr('href', object.link);
+      });
+    });
+  };
+
+  build_timer = function() {
+    var interval;
+    $.when($('time.countdown').html(get_time_left())).then(function() {
+      $('span.spinner').fadeOut('fast');
+      return $('.flip-front').find('h1, h2, .countdown-container').fadeIn('fast');
+    });
     return interval = setInterval(function() {
-      var days, hours, minutes, seconds;
-      seconds = release.diff(moment(new Date()), 's');
-      days = Math.floor(seconds / 86400);
-      seconds -= days * 86400;
-      hours = Math.floor(seconds / 3600);
-      hours = hours < 10 ? "0" + hours : hours;
-      seconds -= hours * 3600;
-      minutes = Math.floor(seconds / 60);
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds -= minutes * 60;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-      return $('time.countdown').html("" + days + " days " + hours + ":" + minutes + ":" + seconds);
+      return $('time.countdown').html(get_time_left());
     }, 1000);
+  };
+
+  get_time_left = function() {
+    var days, hours, minutes, release, seconds;
+    release = moment($('time.release-date').attr('datetime'));
+    seconds = release.diff(moment(new Date()), 's');
+    days = Math.floor(seconds / 86400);
+    seconds -= days * 86400;
+    hours = Math.floor(seconds / 3600);
+    hours = hours < 10 ? "0" + hours : hours;
+    seconds -= hours * 3600;
+    minutes = Math.floor(seconds / 60);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds -= minutes * 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    return "" + days + " days " + hours + ":" + minutes + ":" + seconds;
   };
 
 }).call(this);
